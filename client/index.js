2cc7ce19692e289ef56656bf2e339a08405872fd
Ethan Lee <ethanlee1@yahoo.com>
2019-06-12 11:51:51 +0800

    verifies form is complete and retrieves data

--- client/index.js
+++ client/index.js
@@ -6,12 +6,36 @@
 //         document.getElementById("status").innerHTML = data.resp;
 //     });
 // });
+// async function main() {
+//   const resp = await fetch('http://localhost:8080/v1/tests');
+//   console.log(resp);
+//   console.log(resp.headers.get('hello'));
+//   // var data = await resp.json();
+//   // document.getElementById("status").innerHTML = data.resp;
+// }
+// main();
+console.log('hello')
+function myfunction() {
+  const emailtest = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-async function main() {
-  const resp = await fetch('http://localhost:8080/v1/tests');
-  console.log(resp);
-  console.log(resp.headers.get('hello'));
-  // var data = await resp.json();
-  // document.getElementById("status").innerHTML = data.resp;
+  var data = document.getElementById('form');
+  var email = data[0].value;
+  var check;
+  if (data[1].checked) {
+    check = data[1].value;
+  } else if (data[2].checked) {
+    check = data[2].value;
+  }
+
+  console.log(check);
+  document.getElementById('form').reset();
+  if (emailtest.test(email) && check) {
+    var d = new Date();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+
+    document.getElementById('text').innerHTML = `${email} ${check} @ ${hours}:${minutes}`;
+  }
+
+
 }
-main();
